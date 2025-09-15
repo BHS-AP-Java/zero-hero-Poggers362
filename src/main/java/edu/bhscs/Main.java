ca78c380a8f99514b6efadbf02597711effe30d5
Linus O <1116892@apps.nsd.org>
2025-09-15 17:00:48 +0000

    Got it to work

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -30,7 +30,7 @@ class Main {
       }
     }
 
-    //Mostly pointless print statements
+    // Mostly pointless print statements
     System.out.println(myNum + "working list!");
     System.out.println("67!" + A + someNumber);
     System.out.print(A + "?" + A + someNumber);
@@ -40,6 +40,24 @@ class Main {
     System.out.println();
     System.out.println();
     System.out.println("okay Im done");
+
+
+    Awesome one = new Awesome("1");
+    Awesome two = new Awesome("2");
+    Awesome three = new Awesome("3");
+    Awesome four = new Awesome("4");
+    Awesome five = new Awesome("5");
+    Awesome six = new Awesome("6");
+
+
+
+    System.out.println(one.getName());
+    System.out.println(two.getName());
+    System.out.println(three.getName());
+    System.out.println(four.getName());
+    System.out.println(five.getName());
+    System.out.println(six.getName());
+
 
   }
 }
