0145c637f213ebddc62716cb5534f067a4b8a8af
Linus O <1116892@apps.nsd.org>
2025-09-16 17:05:17 +0000

    Made while loop

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -8,10 +8,8 @@
  * garbage! EDGE CASE: *
  */
 
-package edu.bhscs;
+ package edu.bhscs;
 
-import java.util.ArrayList;
-import java.util.List;
 
 class Main {
 
@@ -19,45 +17,17 @@ class Main {
 
   public static void main(String[] args) {
 
-    // while loop and list testing
-    int someNumber = 7;
-    List<Integer> myNum = new ArrayList<Integer>(5);
-    while (someNumber < 35) {
-      someNumber++;
-      if (someNumber % 5 == 0) {
-        System.out.println(someNumber);
-        myNum.add(someNumber);
-      }
-    }
+     int someNumber = 67;
 
-    // Mostly pointless print statements
-    System.out.println(myNum + "working list!");
-    System.out.println("67!" + A + someNumber);
-    System.out.print(A + "?" + A + someNumber);
-    System.out.print(A + A + someNumber);
-    System.out.println();
-    System.out.println("this is neet");
-    System.out.println();
-    System.out.println();
-    System.out.println("okay Im done");
+     System.out.println("67!" + A + someNumber);
+     System.out.print(A + "?" + A + someNumber);
+     System.out.print(A + A + someNumber);
+     System.out.println();
+     System.out.println("this is neet");
+     System.out.println();
+     System.out.println();
+     System.out.println("okay Im done");
 
 
-    Awesome one = new Awesome("1");
-    Awesome two = new Awesome("2");
-    Awesome three = new Awesome("3");
-    Awesome four = new Awesome("4");
-    Awesome five = new Awesome("5");
-    Awesome six = new Awesome("6");
-
-
-
-    System.out.println(one.getName());
-    System.out.println(two.getName());
-    System.out.println(three.getName());
-    System.out.println(four.getName());
-    System.out.println(five.getName());
-    System.out.println(six.getName());
-
-
-  }
-}
+   }
+ }
