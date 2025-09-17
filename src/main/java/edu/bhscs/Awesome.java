0579adf116ddfc72d8e6983c96abe65c1e413e26
Linus O <1116892@apps.nsd.org>
2025-09-17 16:29:44 +0000

    Fixed it all

--- src/main/java/edu/bhscs/Awesome.java
+++ src/main/java/edu/bhscs/Awesome.java
@@ -1,19 +1,20 @@
 package edu.bhscs;
 
 public class Awesome {
-  //cool shit here
-  //       |
-  //       |
-  //       V
+  // cool shit here
+  // |
+  // |
+  // V
 
   String name;
 
 
 
-  public Awesome(String name){
+  public Awesome(String name) {
     System.out.println("I am awesome " + name);
     this.name = name;
   }
+
   String getName() {
     return "I am " + this.name;
 
--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -10,13 +10,14 @@
 
 package edu.bhscs;
 
+import java.util.ArrayList;
+import java.util.List;
 
 class Main {
 
   public static final char A = 'a';
 
   public static void main(String[] args) {
-
 
     // while loop and list testing
     int someNumber = 7;
@@ -25,11 +26,11 @@ class Main {
       someNumber++;
       if (someNumber % 5 == 0) {
         System.out.println(someNumber);
-        // make list works myNum.add(int, index someNumber);
+        myNum.add(someNumber);
       }
     }
 
-    //Mostly pointless print statements
+    // Mostly pointless print statements
     System.out.println(myNum + "working list!");
     System.out.println("67!" + A + someNumber);
     System.out.print(A + "?" + A + someNumber);
@@ -40,6 +41,25 @@ class Main {
     System.out.println();
     System.out.println("okay Im done");
 
+
+    Awesome one = new Awesome("1");
+    Awesome two = new Awesome("2");
+    Awesome three = new Awesome("3");
+    Awesome four = new Awesome("4");
+    Awesome five = new Awesome("5");
+    Awesome six = new Awesome("6");
+
+
+
+    System.out.println(one.getName());
+    System.out.println(two.getName());
+    System.out.println(three.getName());
+    System.out.println(four.getName());
+    System.out.println(five.getName());
+    System.out.println(six.getName());
+
+
   }
 }
+
 
