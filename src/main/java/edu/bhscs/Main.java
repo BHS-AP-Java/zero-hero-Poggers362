06bed681930ded1b42b000974069c59fef517b4f
Linus O <1116892@apps.nsd.org>
2025-09-17 08:34:18 -0700

    Loops & Lists
    Added a while loop and the use of a list

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -4,32 +4,42 @@
 // 9/12/2025
 
 /*
- * DESCRIPTION: Whatever Mr. Reiber commands
- * INPUT: dunno what this is about yet
- * OUTPUT: print garbage!
- * EDGE CASE: *
+ * DESCRIPTION: Whatever Mr. Reiber commands INPUT: dunno what this is about yet OUTPUT: print
+ * garbage! EDGE CASE: *
  */
 
- package edu.bhscs;
+package edu.bhscs;
 
+import java.util.ArrayList;
+import java.util.List;
 
- class Main {
+class Main {
 
   public static final char A = 'a';
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
-     int someNumber = 67;
+    // while loop and list testing
+    int someNumber = 7;
+    List<Integer> myNum = new ArrayList<Integer>(5);
+    while (someNumber < 35) {
+      someNumber++;
+      if (someNumber % 5 == 0) {
+        System.out.println(someNumber);
+        myNum.add(someNumber);
+      }
+    }
 
-     System.out.println("67!" + A + someNumber);
-     System.out.print(A + "?" + A + someNumber);
-     System.out.print(A + A + someNumber);
-     System.out.println();
-     System.out.println("this is neet");
-     System.out.println();
-     System.out.println();
-     System.out.println("okay Im done");
+    //Mostly pointless print statements
+    System.out.println(myNum + "working list!");
+    System.out.println("67!" + A + someNumber);
+    System.out.print(A + "?" + A + someNumber);
+    System.out.print(A + A + someNumber);
+    System.out.println();
+    System.out.println("this is neet");
+    System.out.println();
+    System.out.println();
+    System.out.println("okay Im done");
 
-
-   }
- }
+  }
+}
